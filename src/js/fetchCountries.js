5a857ab70bac96dff754dc9753b29799acb4b91d
Jimjimka <106761382+Jimjimka@users.noreply.github.com>
2022-11-16 15:18:25 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,6 @@
 const BASE_URL= 'https://restcountries.com/v2/name/'
 
 export const fetchCountries = (name)=>{
-        // fetch(`https://restcountries.com/v3.1/name/${event.target.value}`)
    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
 .then(response=>{
     if(!response.ok){
